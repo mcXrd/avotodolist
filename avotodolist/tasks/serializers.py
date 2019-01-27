747bed312f8ef510a5497a21ad71dad15e7bcea9
mcXrd <matejka.vaclav@gmail.com>
2019-01-27 03:28:29 +0100

    tests added, readme pollished

--- avotodolist/tasks/serializers.py
+++ avotodolist/tasks/serializers.py
@@ -3,6 +3,7 @@ from tasks.models import Task
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Task
         fields = ('id', 'label', 'completed', 'parent', 'is_subtask')
--- avotodolist/tasks/tests.py
+++ avotodolist/tasks/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+client = APIClient()
+
+
+class TaskTestCase(TestCase):
+    """
+    caveat: Testing only subtask related logic because other tests would mean just testing DjangoRestFramework
+    """
+
+    def test_subtask_created_successfuly(self):
+        task_list_url = reverse('task-list')
+        parent_task_response = client.post(task_list_url, {'label': 'dummy parent label text'}, format='json')
+        assert parent_task_response.json()['is_subtask'] == False
+        parent_task_id = parent_task_response.json()['id']
+        parent_url = reverse('task-detail', args=[parent_task_id])
+
+        subtask_response = client.post(task_list_url, {'label': 'dummy sub label text', 'parent': parent_url},
+                                       format='json')
+        assert subtask_response.json()['is_subtask'] == True
